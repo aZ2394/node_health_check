d2a09fd5af3fc210cfd11573d511f2a782133ee7
Ubuntu <ubuntu@ip-172-31-23-198.us-east-2.compute.internal>
2022-05-09 11:43:15 +0000

    add new version

--- index.js
+++ index.js
@@ -9,48 +9,38 @@ const Web3 = require('web3');
 const serverPort = 3001;
 
 
+const options = {
+  // Enable auto reconnection
+  reconnect: {
+      auto: true,
+      delay: 5000, // ms
+      maxAttempts: 5,
+      onTimeout: false
+  }
+};
+const web3_provider = new Web3.providers.HttpProvider("http://localhost:4444",options);
+const web3S_provider = new Web3.providers.WebsocketProvider("ws://localhost:4445/websocket",options);
+const web3Iov_provider = new Web3.providers.HttpProvider('https://public-node.rsk.co',options);
 
+let web3 = new Web3(web3_provider);
+let web3S = new Web3(web3S_provider);
+let web3Iov = new Web3(web3Iov_provider);
 
+console.log('fin q')
 
 http.listen(serverPort, () => {
     console.log('listening on *:'+serverPort);
    });
 
 app.get('/', async (req, res)=> {
-      const web3_provider = new Web3.providers.HttpProvider("http://localhost:4444",{
-        auto: true,
-        delay: 5000, // ms
-        maxAttempts: 5,
-        onTimeout: false,
-      });
-      const web3S_provider = new Web3.providers.WebsocketProvider("ws://localhost:4445/websocket",{
-        auto: true,
-        delay: 5000, // ms
-        maxAttempts: 5,
-        onTimeout: false,
-      });
-      const web3Iov_provider = new Web3.providers.HttpProvider('https://public-node.rsk.co',{
-        auto: true,
-        delay: 5000, // ms
-        maxAttempts: 5,
-        onTimeout: false,
-      });
-     try{  
-        var web3 = new Web3(web3_provider);
-        var web3S = new Web3(web3S_provider);
-        var web3Iov = new Web3(web3Iov_provider);
-        
-        console.log('fin q')
-        
+
+     try{          
         const rpc = await web3.eth.getBlockNumber();
-        console.log('1')
         const ws = await web3S.eth.getBlockNumber();
-        console.log('2')
         const iov = await web3Iov.eth.getBlockNumber();
-        console.log('3')
         const syncResult = new Date(Date.now())+ " processed blocks: rpc "+rpc+", wss: "+ws+" iov: "+iov+ "\n";
         console.log(syncResult);
-	   if(Math.abs(iov - rpc) <= 3 ) {
+	   if(Math.abs(rpc - ws) <= 3 && Math.abs(iov - rpc) <=3 ) {
             return res.status(200).send(syncResult);
           } 
         else if(rpc > iov ) {
@@ -62,10 +52,13 @@ app.get('/', async (req, res)=> {
      }  
      catch(err){
        console.log(err);
-       await web3.setProvider(web3_provider)
-       await web3S.setProvider(web3S_provider)
-       await web3Iov.setProvider(web3Iov_provider)
-       return res.status(504).send("rsk node not responding: " + err + "\n");
+       if(err = 'Error: connection not open on send()')
+          {
+           return res.status(502).send("rsk node not responding: " + err + "\n");
+          }
+       else{
+           return res.status(504).send("rsk node not responding: " + err + "\n");
+       }
     }
 });
 
