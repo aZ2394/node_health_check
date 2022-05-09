8ec64289deafbbd7c9d83b5aaac684e935e1da02
Ubuntu <ubuntu@ip-172-31-23-198.us-east-2.compute.internal>
2022-05-09 17:13:41 +0000

    fix fin

--- index.js
+++ index.js
@@ -4,12 +4,36 @@
 */
 const express= require('express');
 const app = express();
-const http = require('http').createServer(app);
+const http = require('http')
 const Web3 = require('web3');
 const serverPort = 3001;
+const server = http.createServer(app);
 
 
-const options = {
+const http_options = {
+  keepAlive: true,
+  timeout: 20000, // milliseconds,
+  headers: [{name: 'Access-Control-Allow-Origin', value: '*'}],
+  withCredentials: false,
+  agent: {http: http.Agent('health-check'), baseUrl: 'http://localhost'},
+  // Enable auto reconnection
+    reconnect: {
+      auto: true,
+      delay: 5000, // ms
+      maxAttempts: 5,
+      onTimeout: false
+  }
+};
+  
+
+const ws_options = {
+  timeout: 30000, // ms
+  clientConfig: {
+    // Useful to keep a connection alive
+    keepalive: true,
+    keepaliveInterval: 60000 // ms
+  },
+
   // Enable auto reconnection
   reconnect: {
       auto: true,
@@ -18,28 +42,29 @@ const options = {
       onTimeout: false
   }
 };
-const web3_provider = new Web3.providers.HttpProvider("http://localhost:4444",options);
-const web3S_provider = new Web3.providers.WebsocketProvider("ws://localhost:4445/websocket",options);
-const web3Iov_provider = new Web3.providers.HttpProvider('https://public-node.rsk.co',options);
 
-let web3 = new Web3(web3_provider);
-let web3S = new Web3(web3S_provider);
-let web3Iov = new Web3(web3Iov_provider);
 
-console.log('fin q')
 
-http.listen(serverPort, () => {
+server.listen(serverPort, () => {
     console.log('listening on *:'+serverPort);
    });
 
 app.get('/', async (req, res)=> {
-
+      const web3_provider = new Web3.providers.HttpProvider("http://localhost:4444",http_options);
+      const web3S_provider = new Web3.providers.WebsocketProvider("ws://localhost:4445/websocket",ws_options);
+      const web3Iov_provider = new Web3.providers.HttpProvider('https://public-node.rsk.co',http_options);
      try{          
+        var web3 = new Web3(web3_provider);
+        var web3S = new Web3(web3S_provider);
+        var web3Iov = new Web3(web3Iov_provider);
         const rpc = await web3.eth.getBlockNumber();
         const ws = await web3S.eth.getBlockNumber();
         const iov = await web3Iov.eth.getBlockNumber();
         const syncResult = new Date(Date.now())+ " processed blocks: rpc "+rpc+", wss: "+ws+" iov: "+iov+ "\n";
         console.log(syncResult);
+        await web3.currentProvider.disconnect();
+        await web3S.currentProvider.disconnect();
+        await web3Iov.currentProvider.disconnect();
 	   if(Math.abs(rpc - ws) <= 3 && Math.abs(iov - rpc) <=3 ) {
             return res.status(200).send(syncResult);
           } 
@@ -52,13 +77,11 @@ app.get('/', async (req, res)=> {
      }  
      catch(err){
        console.log(err);
-       if(err = 'Error: connection not open on send()')
-          {
-           return res.status(502).send("rsk node not responding: " + err + "\n");
-          }
-       else{
-           return res.status(504).send("rsk node not responding: " + err + "\n");
-       }
+          await web3.currentProvider.disconnect();
+          await web3S.currentProvider.disconnect();
+          await web3Iov.currentProvider.disconnect();
+         return res.status(504).send("rsk node not responding: " + err + "\n");
+       
     }
 });
 
