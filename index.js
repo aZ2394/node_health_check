de13b10e4ffd14156eb6958de86812f04f435a0c
Ubuntu <ubuntu@ip-172-31-23-156.us-east-2.compute.internal>
2022-06-01 14:06:28 +0000

    Add bsc healthcheck

--- index.js
+++ index.js
@@ -50,36 +50,54 @@ server.listen(serverPort, () => {
    });
 
 app.get('/', async (req, res)=> {
-      const web3_provider = new Web3.providers.HttpProvider("http://localhost:4444",http_options);
-      const web3S_provider = new Web3.providers.WebsocketProvider("ws://localhost:4445/websocket",ws_options);
-      const web3Iov_provider = new Web3.providers.HttpProvider('https://public-node.rsk.co',http_options);
-     try{          
-        var web3 = new Web3(web3_provider);
-        var web3S = new Web3(web3S_provider);
-        var web3Iov = new Web3(web3Iov_provider);
-        const rpc = await web3.eth.getBlockNumber();
-        const ws = await web3S.eth.getBlockNumber();
-        const iov = await web3Iov.eth.getBlockNumber();
-        const syncResult = new Date(Date.now())+ " processed blocks: rpc "+rpc+", wss: "+ws+" iov: "+iov+ "\n";
-        await web3.currentProvider.disconnect();
-        await web3S.currentProvider.disconnect();
-        await web3Iov.currentProvider.disconnect();
-	   if(Math.abs(rpc - ws) <= 3 && Math.abs(iov - rpc) <=3 ) {
-            return res.status(200).send(syncResult);
-          } 
-        else if(rpc > iov ) {
-            return res.status(200).send(syncResult);
-          }    
-        else {
-            return res.status(503).send("not in sync - " + syncResult);
-        } 
-     }  
+    const web3_provider = new Web3.providers.HttpProvider("http://localhost:8545",http_options);
+    const web3WS_provider = new Web3.providers.WebsocketProvider("ws://localhost:8546",ws_options);
+    const web3BSN_0_provider = new Web3.providers.HttpProvider('https://bsc-dataseed.binance.org',http_options);
+    const web3BSN_1_provider = new Web3.providers.HttpProvider('https://bsc-dataseed1.defibit.io',http_options);
+    const web3BSN_2_provider = new Web3.providers.HttpProvider('https://bsc-dataseed1.ninicoin.io/',http_options);
+    try {
+         var web3 = new Web3(web3_provider);
+         var web3WS = new Web3(web3WS_provider);
+         var web3public0 = new Web3(web3BSN_0_provider);
+         var web3public1 = new Web3(web3BSN_1_provider);
+         var web3public2 = new Web3(web3BSN_2_provider);
+         const rpc = await web3.eth.getBlockNumber();
+         const ws = await web3WS.eth.getBlockNumber();
+	 try {
+             var web3_public = await web3public0.eth.getBlockNumber();
+	 } catch {
+	     try {
+                 var web3_public = await web3public1.eth.getBlockNumber();
+	     } catch {
+		 try {
+		     var web3_public = await web3public2.eth.getBlockNumber();
+		 } catch {
+		     var web3_public = 0;
+                 }
+	     }
+	 }
+         const syncResult = new Date(Date.now())+ " processed blocks: rpc "+rpc+", wss: "+ws+" iov: "+web3_public+ "\n";
+         await web3.currentProvider.disconnect();
+         await web3WS.currentProvider.disconnect();
+         await web3public0.currentProvider.disconnect();
+         await web3public1.currentProvider.disconnect();
+         await web3public2.currentProvider.disconnect();
+         if(Math.abs(rpc - ws) <= 3 && Math.abs(web3_public - rpc) <=3 ) {
+             return res.status(200).send(syncResult);
+         }
+         else if(rpc > web3_public) {
+             return res.status(200).send(syncResult);
+         }
+         else {
+             return res.status(503).send("not in sync - " + syncResult);
+         }
+     }
      catch(err){
-          await web3.currentProvider.disconnect();
-          await web3S.currentProvider.disconnect();
-          await web3Iov.currentProvider.disconnect();
+         await web3.currentProvider.disconnect();
+         await web3WS.currentProvider.disconnect();
+         await web3public0.currentProvider.disconnect();
+         await web3public1.currentProvider.disconnect();
+         await web3public2.currentProvider.disconnect();
          return res.status(504).send("rsk node not responding: " + err + "\n");
-       
     }
 });
-
